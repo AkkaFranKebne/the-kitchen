a2db420af3948c25fcea03b80aeb07af1ff0578e
AkkaFranKebne <ewawrobel@gmail.com>
2017-10-18 14:16:11 +0200

    passing info about recipe detailes through recipe-list, recipe-item and recipe-detail using  @Input and data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [  //array of recipes (see recipe.model.ts)
         new Recipe('Yummy Chocolate Cookies', 'Gummi bears liquorice apple pie cake biscuit sweet apple pie. Sweet roll jujubes jujubes sugar plum gummi bears lollipop sweet. Gummies pie cheesecake halvah dessert.', 'https://sugarspunrun.com/wp-content/uploads/2017/05/Chocolate-Chip-Cookie-Recipe-1-of-1.jpg'),     //calls the constructor in Recipe class
         new Recipe('Smooth Brownie Cupcakes', 'Gingerbread caramels biscuit toffee. Icing sweet topping. Pie chocolate cake wafer dessert lollipop. Icing ice cream pie bonbon cake muffin toffee brownie cupcake.', 'http://img.taste.com.au/wJW2WMnE/taste/2016/11/chocolate-cupcakes-70267-1.jpeg')
@@ -16,5 +18,10 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
+  }
+
 
 }
