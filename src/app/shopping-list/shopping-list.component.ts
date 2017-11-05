9d04d95544f12a9d66b16e37a95a5082edc81a4b
AkkaFranKebne <ewawrobel@gmail.com>
2017-11-05 11:06:12 +0100

    service for shopping-list that contains the list of ingredients and methods for its' display and change

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,25 +9,19 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-    ingredients: Ingredient[] = [
-        new Ingredient('eggs', 5),
-        new Ingredient('flour', 200),
-        new Ingredient('chocolate', 1),
-        new Ingredient('vanilla', 1),
-        new Ingredient('butter', 250),
-        new Ingredient('raisins', 30),
-        new Ingredient('sugar', 500),
-        new Ingredient('milk', 500)
-    ];
+    ingredients: Ingredient[]; // see shopping-list.service
 
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService ) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getShoppingList();  // displays all shopping list from shopping list service
+    this.shoppingListService.ingredientsChanged.subscribe(   //  listens to the changes in shopping list in shopping list service
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      });
+
   }
 
-  onIngredientAdded(ingredient: Ingredient){
-      this.ingredients.push(ingredient);
-  }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,28 @@
+import { Ingredient } from '../shared/ingredient.model';
+import {EventEmitter} from "@angular/core";
+
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();  // event on ingredients list
+
+  private ingredients: Ingredient[] = [  // array of ingredients (see ingredient.model.ts), will be used in shoopinglist
+    new Ingredient('eggs', 5),
+    new Ingredient('flour', 200),
+    new Ingredient('chocolate', 1),
+    new Ingredient('vanilla', 1),
+    new Ingredient('butter', 250),
+    new Ingredient('raisins', 30),
+    new Ingredient('sugar', 500),
+    new Ingredient('milk', 500)
+  ];
+
+  getShoppingList(){
+    return this.ingredients.slice();  // exact copy
+  }
+
+  addIngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);  // function that add new ingredient to the list, it is initalized in shopping-list-edit
+    this.ingredientsChanged.emit(this.ingredients.slice()); // push the info that the orginal ingredients array has changed
+  }
 
 
 }
